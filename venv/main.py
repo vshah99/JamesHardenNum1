98e509bf38371c6d765347630fe14adcfca672c0
vedantshah <vedant.shah@mail.utoronto.ca>
2019-02-15 21:39:12 -0500

    first

---
+++ venv/main.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from datetime import timedelta
+from iexfinance.stocks import get_historical_data
+from iexfinance.stocks import get_historical_intraday
+import matplotlib.pyplot as plt
+import pandas as pd
+
+end = datetime(2018, 12, 10)
+drop_list = ['average', 'changeOverTime', 'date', 'high', 'label', 'low', 'close',
+             'marketLow', 'marketHigh', 'marketNotional', 'marketOpen', 'marketVolume',
+             'marketNumberOfTrades', 'numberOfTrades', 'open', 'volume',
+             'marketAverage', 'marketChangeOverTime', 'notional']
+
+list_of_df = []
+i = 0
+while (end != datetime.today()):
+    try:
+        df = get_historical_intraday("GOOG", end, output_format = 'pandas')
+        df.drop(drop_list, axis=1, inplace = True)
+        df.rename(columns = {'marketClose': end}, inplace = True)
+        df.to_csv('temp.csv')
+        df = pd.read_csv('temp.csv')
+        df.set_index('minute', inplace=True)
+        list_of_df += [df]
+        end += timedelta(days=1)
+    except:
+        end += timedelta(days=1)
+
+result = pd.concat(list_of_df, sort=False, axis=1)
+result.to_csv('final.csv')
+
