bfa7892718ddab2663fad9591cb31d8f36ec896a
vedantshah <vedant.shah@mail.utoronto.ca>
2019-02-16 14:50:30 -0500

    remove lsten

--- venv/weighted_av.py
+++ venv/weighted_av.py
@@ -64,25 +64,7 @@ def weight_av_intra(ticker):
     final_dict_total.to_csv(file_name)
 
 
-#if __name__ == 'main':
-
-print('test')
-
-# [START gae_python37_app]
-from flask import Flask
-
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
-app = Flask(__name__)
-
-
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-    weight_av_intra('AMZN')
+weight_av_intra('TSLA')
 
 
 
